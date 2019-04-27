88219a351f0ac5c85aabc9771f475443ffc8c635
Graham Ritter <gmr50@georgetown.edu>
2019-04-27 12:43:57 -0400

    working v1

--- web_app/routes/home.py
+++ web_app/routes/home.py
@@ -1,10 +1,10 @@
 
-from flask import Blueprint, request, render_template, redirect, session
+from flask import Blueprint, request, render_template, redirect, session, url_for, flash
 #from web_app.spotify_methods import prompt_for_user_token_override
-from web_app.spotify_methods import prompt_token_flask, get_user_playlists
+from web_app.spotify_methods import prompt_token_flask, get_user_playlists, add_playlist_for_seed, add_tracks_for_seed, add_top_tracks_for_seed, build_playlist, execute_playlist, playlist_unfollow
 
 
-
+import spotipy
 
 # import os
 # import sys
@@ -21,6 +21,15 @@ home_routes = Blueprint("home_routes", __name__)
 def index():
     print("VISITING THE INDEX PAGE")
     #return "You have visited the homepage"
+
+
+    #declaring session variable to send playlist seeds to
+    playlists_list = []
+    tracks_list = []
+    session['playlists_list'] = playlists_list
+    session['tracks_list'] = tracks_list
+
+
     return render_template("index.html")
 
 # GET /hello
@@ -47,14 +56,22 @@ def Login(name=None):
     return render_template("login.html")
 
 
-@home_routes.route("/login/create/")
-def Execute(code=None):
+
+
+@home_routes.route("/login/create/", methods = ['GET', 'POST'])
+def Execute(username=None):
     print("im Executing login script!!!")
 
-    username_hard = "gmr0678"
+    #user_id = request.values.get('username')
 
-    user_id = "gmr0678"
-    auth_url = prompt_token_flask(user_id).get_authorize_url() #> 'https://accounts.spotify.com/authorize?client_id=_____&response_type=code&redirect_uri=________&scope=playlist-modify-private+playlist-read-private'
+    #user_id = "gmr0678"
+
+    username = request.args["username"]
+    print(username)
+
+    session['username'] = username
+
+    auth_url = prompt_token_flask(username).get_authorize_url() #> 'https://accounts.spotify.com/authorize?client_id=_____&response_type=code&redirect_uri=________&scope=playlist-modify-private+playlist-read-private'
     return redirect(auth_url)
 
     app.run(debug=True)
@@ -64,7 +81,8 @@ def Execute(code=None):
 @home_routes.route("/callback/")
 def Callback(code=None):
 
-    user_id = "gmr0678"
+    #user_id = "gmr0678"
+    user_id = session.get('username', None)
 
     #gets authorization code from url
     print("SPOTIFY CALLBACK")
@@ -82,16 +100,25 @@ def Callback(code=None):
 
         session['token_var'] = token
 
+
+        
+
         return redirect("/builder")
     else:
         message = "OOPS, UNABLE TO GET CODE"
         print(message)
         return message
 
-@home_routes.route("/builder/")
-def SeedBuilder():
 
-    user_id = "gmr0678"
+
+@home_routes.route("/builder/", methods=['POST','GET'])
+def seedbuilder():
+
+
+    ranges = [['short term', 'short_term'], ['medium term', 'medium_term'], ['long term', 'long_term']]
+
+    #user_id = "gmr0678"
+    user_id = session.get('username', None)
 
     try:
         builder_token = session.get('token_var', None)
@@ -101,14 +128,161 @@ def SeedBuilder():
     if(builder_token):
 
         user_playlists = get_user_playlists(builder_token, user_id)
-        print("render *********")
-        print(user_playlists)
 
-        return render_template("builder.html", playlists = user_playlists)
+        return render_template("builder.html", playlists = user_playlists, ranges = ranges)
 
 
 
 
     else:
         print("no token")
+        return render_template("no_token.html")
+
+
+
+@home_routes.route("/builder_redirect/", methods=['GET'])
+def redirect_builder():
+    
+    session['tracks_list'] = []
+
+    return redirect("/builder/")
+
+
+
+@home_routes.route("/builder/addseed/", methods=['POST'])
+def AddSeed():
+
+
+    try:
+        token = session.get('token_var', None)
+    except:
+        print("failed to get session variable")
+
+
+    selected_playlist_tracks = []
+    selection = request.form.get('playlist_seed')
+
+    user_id = session.get('username', None)
+
+    tracks_list = session.get('tracks_list', None)
+    selected_playlist_tracks = add_tracks_for_seed(user_id, selection, token)
+    tracks_list = tracks_list + selected_playlist_tracks
+    session['tracks_list'] = tracks_list
+    print(tracks_list)
+
+    #playlist_tracks = sp.user_playlist_tracks(user_id,selection)
+
+
+    # playlists_list = session.get('playlists_list', None)
+    # playlists_list = add_playlist_for_seed(selection, playlists_list)
+
+    # session['playlists_list'] = playlists_list
+    # print(playlists_list)
+
+
+    return redirect('/builder/')
+
+
+
+
+
+@home_routes.route("/builder/addtoptracksseed/", methods = ['POST'])
+def add_top_tracks():
+
+    tracks_list = session.get('tracks_list', None)
+
+    try:
+        token = session.get('token_var', None)
+    except:
+        print("failed to get session variable")
+
+    selection = request.form.get('term_seed')
+    print(selection)
+
+    tracks_list = tracks_list + add_top_tracks_for_seed(selection, token)
+    session['tracks_list'] = tracks_list
+    print(tracks_list)
+
+
+
+    return redirect('/builder/')
+
+
+
+@home_routes.route("/builder/create_playlist/")
+def playlist_builder():
+    
+    try:
+        token = session.get('token_var', None)
+    except:
+        print("failed to get session variable")
+
+
+    tracks_list = session.get('tracks_list', None)
+    print("in flash route")
+
+
+    if(tracks_list == []):
+        flash('You need to add tracks to seed! Get planting!')
+        return redirect("/builder/")
+    else:
+        recommendations = build_playlist(tracks_list, token)
+        session['recommendations'] = recommendations
+        print(recommendations)
+
+        return redirect("/builder/create_playlist/name_tree")
+
+
+
+
+
+@home_routes.route("/builder/create_playlist/name_tree")
+def name_playlist():
+
+    return render_template("plant_tree.html")
+
+
+
+
+
+
+@home_routes.route("/builder/create_playlist/thank_you", methods=['GET','POST'])
+def thank_you(playlist_name = None):
+
+    try:
+        token = session.get('token_var', None)
+    except:
+        print("failed to get session variable")
+
+
+
+    playlist_name = request.args["playlist_name"]
+    username = session.get('username', None)
+    recommendations = session.get('recommendations', None)
+
+    #still need to get playlistname
+
+
+    #playlist_name = request.values.get('playlist_name') 
+    print(playlist_name)
+
+    message = execute_playlist(token, username, recommendations, playlist_name)
+    print(message)
+    message = playlist_unfollow(username, token)
+    print(message)
+
+
+
+
+
+    return render_template("thank_you.html")
+
+
+
+
+
+#Nnot working
+@home_routes.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
--- web_app/spotify_methods.py
+++ web_app/spotify_methods.py
@@ -3,103 +3,9 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import spotipy
 import spotipy.util as util 
 from flask import Flask, render_template, request, redirect, Blueprint
+import random
+import datetime 
 
-
-
-def prompt_for_user_token_override(username, scope=None, client_id = None,
-        client_secret = None, redirect_uri = None, cache_path = None):
-    ''' prompts the user to login if necessary and returns
-        the user token suitable for use with the spotipy.Spotify 
-        constructor
-        Parameters:
-         - username - the Spotify username
-         - scope - the desired scope of the request
-         - client_id - the client id of your app
-         - client_secret - the client secret of your app
-         - redirect_uri - the redirect URI of your app
-         - cache_path - path to location to save tokens
-    '''
-
-    if not client_id:
-        client_id = os.getenv('SPOTIPY_CLIENT_ID')
-
-    if not client_secret:
-        client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-
-    if not redirect_uri:
-        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
-
-    if not client_id:
-        print('''
-            You need to set your Spotify API credentials. You can do this by
-            setting environment variables like so:
-            export SPOTIPY_CLIENT_ID='your-spotify-client-id'
-            export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
-            export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
-            Get your credentials at     
-                https://developer.spotify.com/my-applications
-        ''')
-        raise spotipy.SpotifyException(550, -1, 'no credentials set')
-
-    cache_path = cache_path or ".cache-" + username
-    sp_oauth = SpotifyOAuth(client_id, client_secret, redirect_uri, 
-        scope=scope, cache_path=cache_path)
-
-    # try to get a valid token for this user, from the cache,
-    # if not in the cache, the create a new (this will send
-    # the user to a web page where they can authorize this app)
-
-    token_info = sp_oauth.get_cached_token()
-
-    if not token_info:
-        print('''
-            User authentication requires interaction with your
-            web browser. Once you enter your credentials and
-            give authorization, you will be redirected to
-            a url.  Paste that url you were directed to to
-            complete the authorization.
-        ''')
-        auth_url = sp_oauth.get_authorize_url()
-        print(auth_url + "**************")
-        try:
-            import webbrowser
-            webbrowser.open(auth_url)
-            print("Opened %s in your browser" % auth_url)
-        except:
-            print("Please navigate here: %s" % auth_url)
-
-
-        print()
-        print()
-
-        #os.environ['sp_oauth'] = sp_oauth
-
-
-
-        #try:
-             #response = webbrowser.get(auth_url)
-           
-
-        #except NameError:
-
-             #response = webbrowser.get(auth_url)
-            
-        #     response = input("Enter the URL you were redirected to: ")
-
-
-    #     code = sp_oauth.parse_response_code(response)
-    #     token_info = sp_oauth.get_access_token(code)
-    # # Auth'ed API request
-    # if token_info:
-    #     return token_info['access_token']
-    # else:
-    #     return None
-
-    #     print("success!")
-
-    #session['spotify_auth'] = sp_oauth
-
-    return sp_oauth
 
 
 
@@ -109,22 +15,23 @@ def prompt_token_flask(user_id):
 
 
 
-    username = "gmr0678"
     username = user_id
+
     
 
-    scope = 'user-top-read playlist-modify-private'
+    scope = 'user-top-read playlist-modify-public'
     client_id_saved = os.environ.get("CLIENT_ID", "Oops, please set env var called 'CLIENT_ID'")
     client_secret_saved = os.environ.get("CLIENT_SECRET", "Oops, please set env var called  'CLIENT_SECRET")
     redirect_uri_saved = "http://localhost:5000/callback/"
-    path = (os.path.normpath(os.getcwd()) + "web_app/caches/.cache-" + username)
+    path = (os.path.normpath(os.getcwd()) + "/web_app/caches/.cache-" + username)
+
 
 
     return SpotifyOAuth(
         client_id=client_id_saved,
         client_secret=client_secret_saved,
         redirect_uri=redirect_uri_saved,
-        scope='user-top-read playlist-modify-private',
+        scope=scope,
         cache_path=path
     )
 
@@ -146,25 +53,162 @@ def get_user_playlists(token, user_id):
 
     n=1
     
-
+    array_list = [] 
     id_array = []
 
     
-    print("Which playlist would you like to pick?")
+    #print("Which playlist would you like to pick?")
     for item_get in playlists['items']:
 
         #namestr = item_get['items']['name']
         #tracksstr = item_get['items']['tracks']
         namestr = item_get['name']
         playlist_item = item_get['id']
-        print(str(n) + ' ' + namestr + ' ' + playlist_item)
-        id_array.append(namestr)
+        #print(str(n) + ' ' + namestr + ' ' + playlist_item)
+
+
+        array_list = [namestr, playlist_item]
+        id_array.append(array_list)
+
         n = n+ 1
 
 
-    print(id_array)
+    #print(id_array)
+
+
 
     return id_array
+
+
+def add_playlist_for_seed(input, playlists_list):
+    
+    playlists_list.append(input)
+
+    print(playlists_list)
+    return playlists_list
+
+
+
+def add_tracks_for_seed(username, selected_playlist, token):
+    tracks_list = []
+
+
+    sp = spotipy.Spotify(auth=token)
+    playlist_tracks = sp.user_playlist_tracks(username,selected_playlist)
+
+    for track in playlist_tracks['items']:
+        tracks_list.append(track['track']['id'])
+
+    return tracks_list
+
+
+
+def add_top_tracks_for_seed(track_range, token):
+
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_top_tracks(time_range=track_range, limit=50)
+    id_array = []
+
+    
+    for i, item in enumerate(results['items']):
+        print(i, item['name'], '//', item['artists'][0]['name'])
+        id_array.append(item['id'])
+
+    return id_array
+
+
+
+def build_playlist(tracks, token):
+    sp = spotipy.Spotify(auth=token)
+
+    tracks = random.sample(tracks, len(tracks))
+
+    n = 3
+    x = 0
+    #https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+    sublist = [tracks[i * n:(i + 1) * n] for i in range((len(tracks) + n - 1) // n )] 
+
+
+    rec_list = []
+
+    while x < len(sublist):
+
+        recomends = sp.recommendations(seed_tracks=list(sublist[x]), limit=3)
+        #pprint.pprint(recomends)
+    
+
+        rec_list.append(recomends['tracks'][0]['id'])
+        print(recomends['tracks'][0]['name'])
+        rec_list.append(recomends['tracks'][1]['id'])
+
+        
+        x = x+1
+    return(rec_list)
+
+
+def execute_playlist(token, username, recommendations, playlist_name):
+
+
+
+    now = datetime.datetime.now()
+    timestring = now.strftime("%H//%M//%S//%f")
+    print(timestring)
+    sp = spotipy.Spotify(auth=token)
+
+    playlist_name = ("GMR HIST MCHN: " + playlist_name + " " + timestring)
+    playlist_description = ("GMR's HISTORY MACHINE working project playlist")
+    print(playlist_name)
+    #creates playlist
+    #https://github.com/plamere/spotipy/blob/master/examples/create_playlist.py
+    playlists = sp.user_playlist_create(username, playlist_name, public = True, description = playlist_description)
+
+
+    playlist_id = playlists['id']
+
+
+    sp.user_playlist_add_tracks(username, playlist_id, recommendations, position=None)
+
+    message = "success"
+    return message
+
+def playlist_unfollow(user, token):
+
+
+    sp = spotipy.Spotify(auth=token)
+    playlists = sp.user_playlists(user)
+    name_array = [] 
+    id_array = []
+    x = 0
+
+    for item in playlists["items"]:
+
+        name_array.append(item["name"])
+        id_array.append(item["id"])
+
+
+    gmr_list = []
+    gmr_id = []
+
+    for item, id_code in zip(name_array, id_array):
+        result = item.startswith("GMR HIST MCHN")
+        if(result):
+            gmr_list.append(item)
+            gmr_id.append(id_code)
+
+
+
+
+    message = "nothing deleted"
+    if(len(gmr_id) > 2):
+
+        gmr_id[-1]
+        sp.user_playlist_unfollow(user, gmr_id[-1])
+        message = "deleted"
+
+
+
+    return message
+
 
 
 
